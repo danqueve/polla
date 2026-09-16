eeca736998789af5c18839cd6404e34f8d2b14f1
danqueve <danqueve@gmail.com>
2026-09-16 17:59:46 -0300

    Ajuste en plantilla de configuración de base de datos

--- config/db.example.php
+++ config/db.example.php
@@ -9,7 +9,7 @@
 
 define('DB_HOST',    'localhost');
 define('DB_PORT',    '3306');
-define('DB_NAME',    'polla_quevedo');
+define('DB_NAME',    'danqueve_polla');
 define('DB_USER',    'root');
 define('DB_PASS',    '');
 define('DB_CHARSET', 'utf8mb4');
